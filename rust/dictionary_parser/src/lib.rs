462c18c98afa4d858a9bc5c3d238245c05d05ec5
Kagami Sascha Rosylight <saschanaz@outlook.com>
2020-11-14 22:07:14 +0100

    refactor(dictionary_parser): reformat

--- rust/dictionary_parser/src/lib.rs
+++ rust/dictionary_parser/src/lib.rs
@@ -5,17 +5,32 @@ mod parser;
 use parser::find_items;
 
 #[no_mangle]
-pub unsafe extern fn dictionary_find_items(content: *const c_void, search_key: *const c_void, is_text_search: bool, is_wildcard_search: bool, keys_buffer: *mut *mut c_void, values_buffer: *mut *mut c_void, buffer_length: usize) -> usize {
-    let content = Box::leak(RustStringRange::from_void(content as *mut _));
-    let search_key = Box::leak(RustStringRange::from_void(search_key as *mut _));
+pub unsafe extern "C" fn dictionary_find_items(
+  content: *const c_void,
+  search_key: *const c_void,
+  is_text_search: bool,
+  is_wildcard_search: bool,
+  keys_buffer: *mut *mut c_void,
+  values_buffer: *mut *mut c_void,
+  buffer_length: usize,
+) -> usize {
+  let content = Box::leak(RustStringRange::from_void(content as *mut _));
+  let search_key = Box::leak(RustStringRange::from_void(search_key as *mut _));
 
-    let result = find_items(content.as_slice(), search_key.as_slice(), is_text_search, is_wildcard_search);
+  let result = find_items(
+    content.as_slice(),
+    search_key.as_slice(),
+    is_text_search,
+    is_wildcard_search,
+  );
 
-    let len = std::cmp::min(result.len(), buffer_length);
-    for i in 0..len {
-        *keys_buffer.offset(i as isize) = Box::into_raw(Box::new(RustStringRange::from_str(result[i].0))) as *mut c_void;
-        *values_buffer.offset(i as isize) = Box::into_raw(Box::new(RustStringRange::from_str(result[i].1))) as *mut c_void;
-    }
+  let len = std::cmp::min(result.len(), buffer_length);
+  for i in 0..len {
+    *keys_buffer.offset(i as isize) =
+      Box::into_raw(Box::new(RustStringRange::from_str(result[i].0))) as *mut c_void;
+    *values_buffer.offset(i as isize) =
+      Box::into_raw(Box::new(RustStringRange::from_str(result[i].1))) as *mut c_void;
+  }
 
-    len
+  len
 }
